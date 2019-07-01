124c3c9344f835f6997a43c2a7805a35238f1246
Ankit Samantaray <44136101+AnkitSamantaray@users.noreply.github.com>
2019-07-01 22:31:46 +0100

    Updated App.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,33 +5,20 @@ import './styles.css';
 import React          from 'react';
 import { Link }       from 'react-router';
 import { connect }    from 'react-redux';
-import Helmet         from 'react-helmet';
 
-import { Translate }  from 'react-redux-i18n';
-
-export const NavBar = ({ lang, setLang }: { lang:string; setLang:(lang:string) => void }) => {
-  const nav:link[] = [
-    { href:"/beers", label:"Search Beers" }
-  ];
+export const NavBar = ({}) => {
 
   return (
     <nav className="navbar">
-      { nav.map(( item:link, id:number ) => (
-          <Link key={id}
-                className="navbar_item"
-                activeClassName={'navbar_item-active'}
-                to={item.href}>
-            <Translate value={`NavBar.${item.label}`}/>
-          </Link>
-        ))
-      }
+       <ul className="navbar-nav">
+            <li><Link to={'/beers'} className="nav-link"> Search Beers </Link></li>
+       </ul>
     </nav>
   );
 };
 
-const App = ({ lang, setLocale, children }:{ lang:string; setLocale:(lang:string) => void; children:React$Element<any> }) =>
+const App = ({children }:{children:React$Element<any> }) =>
   <section className="App">
-    <Helmet htmlAttributes={{ lang }}/>
     <NavBar/>
     { children }
   </section>;
